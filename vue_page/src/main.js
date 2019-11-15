754394727bd4b170ffdc60fdfe50efe229ac1e40
dengpengxin <DenPpenXxin@qq.com>
2019-11-15 15:12:12 +0800

    测试提交

--- vue_page/src/main.js
+++ vue_page/src/main.js
@@ -6,11 +6,15 @@ import config from './config'
 import ElementUI from 'element-ui'
 import axios from 'axios'
 
+import Vant from 'vant'
+import 'vant/lib/index.css'
 /** 配置axios */
 import './assets/css/base.css'
 
 /** 配置element-ui */
 import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(Vant)
 Vue.use(ElementUI)
 
 /** 配置axios */
---
+++ vue_page/src/views/Index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="index">
+    <van-search placeholder="请输入搜索关键词" />
+    <h1>立创商城</h1>
+  </div>
+</template>
